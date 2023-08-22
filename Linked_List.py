18a3d913278fe5d6aa4797877ce8e61864f98a58
nima ghanbari <nimagithub1401@gmail.com>
2023-08-22 15:49:29 +0330

    base linklist

---
+++ Linked_List.py
@@ -0,0 +1,9 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        
+class Linked_list:
+    def __init__(self):
+        self.head = None
+        self.length = 0
