8b332e96536d7f2f243d3ca8e16ed1e1d4700975
nima ghanbari <nimagithub1401@gmail.com>
2023-08-22 15:56:13 +0330

    add comment and test

--- Linked_List.py
+++ Linked_List.py
@@ -1,13 +1,22 @@
+#When we use lists, the elements of the list are stored sequentially in memory.
+#When we want to add an element to this list, what actually happens is that if this list is not spaced,
+#it looks for a place in memory that is three times the space of the previous list and forms the new list there.
+#to give Due to these changes, a linked list was generated.
+
+#A linked list is a list whose elements are in the playback memory and are connected by a link.
+#There are different types of linked list.
+
+#This is the element class
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
-        
+#This is a linked list class       
 class Linked_list:
     def __init__(self):
         self.head = None
         self.length = 0
-        
+    #This function adds the received element to the first of the list    
     def push(self, value):
         new_node = Node(value)
         if self.head:
@@ -16,7 +25,7 @@ class Linked_list:
         else:
             self.head = new_node
         self.length += 1
-    
+    #This function adds the received element to the end of the list
     def append(self, value):
         new_node = Node(value)
         if self.head:
@@ -28,7 +37,8 @@ class Linked_list:
         else:
             self.head = new_node
         self.length += 1
-    
+    #This function takes two inputs, one is the index and the other is the value,
+    #and adds the value in the list with the house number of the index.
     def insert(self, index, value):
         if index <= 0:
             self.push(value)
@@ -44,7 +54,7 @@ class Linked_list:
                 new_node = Node(value)
                 new_node.next = temp
                 pre.next = new_node
-                
+    #These two functions are for printing            
     def setter(self, lst):
         self.head = None
         self.length = 0
@@ -59,6 +69,21 @@ class Linked_list:
         else:
             _str += str(temp.data)
         return _str
-    
+    #Returns the number of elements in the list.
     def __len__(self):
         return self.length
+    
+    
+
+ll = Linked_list()
+ll.append(1)
+ll.append(2)
+ll.push(3)
+ll.push(4)
+ll.push(5)
+ll.insert(2,6)
+ll.insert(-10,7)
+ll.insert(100,8)
+
+
+print(ll)
